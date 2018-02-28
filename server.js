a41c9f8e1ad7bb15f01f5f0117de2042257b0789
Guille <guillote_gg@hotmail.com>
2018-02-28 17:30:37 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentDate', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+app.use((req, res) => {
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home',        
+        welcomeMessage: 'Hey, wellcome!'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/bad/', (req, res) => {
+    res.send({
+        errorMessage: 'unable to handle request'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`App listening in ${port} port`);
+});
