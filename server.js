bf60d3bdbe16992309ac56ee906a60fc5efbf394
Guille <guillote_gg@hotmail.com>
2018-03-01 12:58:58 -0300

    adding projects item

--- server.js
+++ server.js
@@ -41,6 +41,12 @@ app.get('/about', (req, res) => {
     res.send('About Page');
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    })
+});
+
 app.get('/bad/', (req, res) => {
     res.send({
         errorMessage: 'unable to handle request'
